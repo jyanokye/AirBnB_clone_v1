0d137e94540dc77613cbdab841d7a661c927cd3f
jyanokye <101642075+jyanokye@users.noreply.github.com>
2022-12-22 10:22:23 +0000

    web

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -4,7 +4,7 @@ import os.path
 import time
 from fabric.operations import run, put, sudo
 from fabric.api import *
-env.hosts = ['35.185.103.0', '35.237.21.105']
+env.hosts = ['54.157.191.136', '54.157.163.118']
 
 
 def do_pack():
